60621114ad9650e3c56c98acbcb11355393eba2b
Jason Rothgeb <jrothgeb@gmail.com>
2022-06-21 10:32:22 -0500

    It works now

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const num1 = 31;
+const num2 = 2;
+let multiply = num1*num2;
+
+
+const random = Math.floor(Math.random()*10)+1;
+console.log(random);
+
+
+const num3 = 19;
+const num4 = 5;
+let mod = num3%num4;
+ 
+
+let max = Math.max(2, 5, 6, 8, 20);
+
+
 
 // Write your code here
